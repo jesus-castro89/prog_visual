c31ea5f19b5cc996ff65f0fd5c0582cf65ec2b20
Jesús Castro <jesus.castro89@gmail.com>
2025-04-11 12:32:00 -0600

    Calculadora

--- src/com/example/sensor/SensorClient.java
+++ src/com/example/sensor/SensorClient.java
@@ -1,15 +1,18 @@
 package com.example.sensor;
 
-import graficas.TemperatureTableModel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.PlotOrientation;
-import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.plot.*;
+import org.jfree.chart.renderer.xy.VectorRenderer;
+import org.jfree.data.Range;
+import org.jfree.data.general.DefaultValueDataset;
 import org.jfree.data.xy.*;
+import org.jfree.ui.StrokeSample;
 
 import javax.swing.*;
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -25,8 +28,14 @@ public class SensorClient extends JFrame {
     private JScrollPane chartOne;
     private JTabbedPane tabbedPane1;
     private JScrollPane chartTwo;
+    private JScrollPane chartThree;
+    private JScrollPane chartFive;
+    private JScrollPane chartSix;
+    private JScrollPane chartSeven;
     private ArrayList<Double> temperatures;
     private ArrayList<Double> windSpeeds;
+    private ArrayList<Double> humidities;
+    private ArrayList<Double> atmosphericPressures;
 
     public static void main(String[] args) {
 
@@ -36,6 +45,8 @@ public class SensorClient extends JFrame {
     public SensorClient() {
         temperatures = new ArrayList<>();
         windSpeeds = new ArrayList<>();
+        humidities = new ArrayList<>();
+        atmosphericPressures = new ArrayList<>();
         setTitle("Sensor Data");
         setSize(900, 600);
         setContentPane(mainPanel);
@@ -58,7 +69,14 @@ public class SensorClient extends JFrame {
                     if (data != null) {
                         temperatures.add(data.getTemperature());
                         windSpeeds.add(data.getWindSpeed());
+                        humidities.add(data.getHumidity());
+                        atmosphericPressures.add(data.getAtmosphericPressure());
                         addChartPanel();
+                        addVelocityChartPanel();
+                        addLineChartPanel();
+                        addWindSpeedPanel();
+                        addBarChartPanel();
+                        addThermometerChartPanel();
                     }
                 } while (data != null);
             } catch (IOException e) {
@@ -99,31 +117,124 @@ public class SensorClient extends JFrame {
         return dataset;
     }
 
-    private WindDataset createWindDataset() {
-        DefaultWindDataset dataset = new DefaultWindDataset();
-        for (Double windSpeed : windSpeeds) {
-            dataset.addWindData(windSpeeds.indexOf(windSpeed) + 1, windSpeed);
+    private XYDataset createWindPolarData() {
+
+        XYSeries series = new XYSeries("Temperatura");
+        for (Double temperature : windSpeeds) {
+            series.add((int) (Math.random() * 360), temperature);
         }
-        WindDataset dataset = new WindDataset();
-        for (Double windSpeed : windSpeeds) {
-            dataset.addWindData(windSpeeds.indexOf(windSpeed) + 1, windSpeed);
-        }
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
         return dataset;
     }
 
-    /*private void addVelocityChartPanel() {
+    private XYBarDataset createHumidityData() {
 
-        JFreeChart chart = ChartFactory.createWindPlot(
-                "Gráfico de Viento",
-                "ID",
-                "Velocidad del Viento",
-                createWindDataset(),
-                PlotOrientation.VERTICAL,
+        XYSeries series = new XYSeries("Humedad");
+        for (Double humidity : humidities) {
+            series.add(humidities.indexOf(humidity) + 1, humidity);
+        }
+        XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+        XYBarDataset barDataset = new XYBarDataset(dataset, 0.5);
+        return barDataset;
+    }
+
+    private VectorSeriesCollection createVectorSeriesCollection() {
+
+        VectorSeries series = new VectorSeries("Viento");
+        series.add(1, 1, -0.5, 0.5);  // Viento hacia el noroeste
+        series.add(1, 2, 0, 1);       // Viento hacia el norte
+        series.add(2, 1, 1, 0);       // Viento hacia el este
+        series.add(2, 2, 0.7, 0.7);    // Viento hacia el noreste
+        VectorSeriesCollection dataset = new VectorSeriesCollection();
+        dataset.addSeries(series);
+        return dataset;
+    }
+
+    private void addWindSpeedPanel() {
+        DefaultValueDataset dataset = new DefaultValueDataset(windSpeeds.getLast());
+        MeterPlot plot = new MeterPlot(dataset);
+        plot.setRange(new Range(0, 20));
+        plot.addInterval(new MeterInterval("Bajo", new Range(0, 5),
+                Color.green, new BasicStroke(0.7f), Color.green));
+        plot.addInterval(new MeterInterval("Medio", new Range(5, 10),
+                Color.yellow, new BasicStroke(0.7f), Color.yellow));
+        plot.addInterval(new MeterInterval("Alto", new Range(10, 20),
+                Color.red, new BasicStroke(0.7f), Color.red));
+        plot.setDialOutlinePaint(Color.BLACK);
+        plot.setDialBackgroundPaint(Color.WHITE);
+        plot.setNeedlePaint(Color.BLUE);
+        plot.setTickLabelsVisible(true);
+        plot.setTickLabelFont(new Font("Arial", Font.BOLD, 12));
+        plot.setTickLabelPaint(Color.DARK_GRAY);
+        plot.setUnits("km/h");
+        plot.setDialShape(DialShape.PIE);
+        JFreeChart chart = new JFreeChart("Velocímetro",
+                JFreeChart.DEFAULT_TITLE_FONT,
+                plot,
+                false);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(400, 300));
+        chartFive.setViewportView(chartPanel);
+    }
+
+    private void addLineChartPanel() {
+
+        JFreeChart chart = ChartFactory.createXYLineChart(
+                "Gráfico de Vectores de Viento",
+                "X", "Y", createVectorSeriesCollection()
+        );
+        XYPlot plot = (XYPlot) chart.getPlot();
+        VectorRenderer renderer = new VectorRenderer();
+        renderer.setSeriesPaint(0, Color.blue);
+        plot.setRenderer(renderer);
+        // Configurar ejes
+        NumberAxis domainAxis = (NumberAxis) plot.getDomainAxis();
+        domainAxis.setRange(0, 3);
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setRange(0, 3);
+        // Configurar el panel
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+        this.chartThree.setViewportView(chartPanel);
+    }
+
+    private void addThermometerChartPanel() {
+
+        DefaultValueDataset dataset = new DefaultValueDataset(temperatures.getLast());
+        ThermometerPlot plot = new ThermometerPlot(dataset);
+        plot.setRange(0, 35);
+        plot.setThermometerStroke(new BasicStroke(2.0f));
+        plot.setSubrange(0, 0, 19);
+        plot.setSubrange(1, 19, 30);
+        plot.setSubrange(2, 30, 35);
+        plot.setUnits(ThermometerPlot.UNITS_CELCIUS);
+        JFreeChart chart = new JFreeChart("Termómetro",
+                JFreeChart.DEFAULT_TITLE_FONT,
+                plot,
+                false);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(400, 300));
+        chartSeven.setViewportView(chartPanel);
+    }
+
+    private void addVelocityChartPanel() {
+
+        JFreeChart chart = ChartFactory.createPolarChart(
+                "Rosa de los Vientos",
+                createWindPolarData(),
                 true,
                 true,
                 false
         );
-    }*/
+        PolarPlot polarPlot = (PolarPlot) chart.getPlot();
+        polarPlot.setAngleGridlinesVisible(true);
+        polarPlot.setRadiusGridlinesVisible(true);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+        this.chartTwo.setViewportView(chartPanel);
+    }
 
     private void addChartPanel() {
 
@@ -133,5 +244,25 @@ public class SensorClient extends JFrame {
         chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
         chartPanel.setMouseWheelEnabled(true);
         this.chartOne.setViewportView(chartPanel);
+    }
+
+    private void addBarChartPanel() {
+        JFreeChart chart = ChartFactory.createXYBarChart(
+                "Gráfico de Barras",
+                "ID",
+                false,
+                "Temperatura",
+                createHumidityData(),
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+        XYPlot plot = (XYPlot) chart.getPlot();
+        NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setRange(0, 90);
+        ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+        this.chartSix.setViewportView(chartPanel);
     }
 }
